d6fdb7ad015b1e07cfd4353a804941c0c08b913b
James <james.wainwright@roadmap-it.co.uk>
2021-03-09 15:26:47 +0000

    Update new_machine.sh
    Updated script with notes

--- new_machine.sh
+++ new_machine.sh
@@ -1,18 +1,38 @@
 #!/usr/bin/env bash
 
+# Installs Homebrew - will install xcode command line tools if missing
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install google-chrome
+# Installs brew casks / apps
+brew install --cask google-chrome
 brew install --cask slack
-brew install --cask microsoft-teams
+brew install --cask microsoft-teams # Micros Teams will prompt for a password to complete install
 brew install --cask zoom
 brew install --cask 1password
 brew install --cask malwarebytes
+
+# This is for the desktop cloud app, from in here you can then install
+# the speci Adobe apps you need to
 brew install --cask adobe-creative-cloud
 
+# checks / upgrades brew casks
 brew upgrade
-
-#/usr/local/bin/brew install [app]
 
 # Uninstall script for Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
+
+# Removes directoires not removed with the Homebrew uninstall script
+# These don't have to be removed, but if you'd like Homebrew wiped off fresh
+rm -r {
+  /usr/local/.com.apple.installer.keep
+  /usr/local/Frameworks/
+  /usr/local/Homebrew/
+  /usr/local/bin/
+  /usr/local/etc/
+  /usr/local/include/
+  /usr/local/lib/
+  /usr/local/opt/
+  /usr/local/sbin/
+  /usr/local/share/
+  /usr/local/var/
+  }
