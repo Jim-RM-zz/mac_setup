a65bf0ca84ff71d6df2705e3c76de3e2392f4161
Jim <65395254+Jim-RM@users.noreply.github.com>
2021-04-15 14:01:13 +0100

    Update bo.sh

--- bo.sh
+++ bo.sh
@@ -6,24 +6,24 @@
 # Installs brew casks / apps
 brew install --cask google-chrome
 brew install --cask slack
-brew install --cask zoom
 brew install --cask 1password
 brew install --cask malwarebytes
 brew install --cask rhino
 brew install --cask keyshot
-brew install --cask microsoft-teams # Micros Teams will prompt for a password to complete install
-brew install --cask microsoft-office # Will prompt for a password to complete install
-
-brew install --cask teamviewer
 
 # Install for plain old Teamviewer is default, but uncomment the line
 # below if you would rather install host.
+brew install --cask teamviewer
 # brew install --cask teamviewer-host
-
 
 # This is for the desktop cloud app, from in here you can then install
 # the specific Adobe apps you need to
 brew install --cask adobe-creative-cloud
+
+# The apps below will ask / require a password to install
+brew install --cask zoom
+brew install --cask microsoft-teams # Micros Teams will prompt for a password to complete install
+brew install --cask microsoft-office # Will prompt for a password to complete install
 
 # checks / upgrades brew casks
 brew upgrade
