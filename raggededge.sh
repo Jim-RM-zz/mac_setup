25d907817850ba1f8732866c8770c91f59297c4f
Jim <65395254+Jim-RM@users.noreply.github.com>
2021-06-28 14:47:57 +0100

    Update raggededge.sh

--- raggededge.sh
+++ raggededge.sh
@@ -9,7 +9,7 @@
 brew install --cask google-chrome
 brew install --cask slack
 brew install --cask figma
-#brew install --cask fontexplorer-x-pro
+brew install --cask fontexplorer-x-pro
 
 # Install for plain old Teamviewer is default, but uncomment the line
 # below if you would rather install host.
@@ -21,7 +21,7 @@ brew install --cask teamviewer
 brew install --cask adobe-creative-cloud
 
 # These installs below will prompt / require a password to install
-#brew install --cask zoom
+brew install --cask zoom
 brew install --cask microsoft-teams # Micros Teams will prompt for a password to complete install
 brew install --cask microsoft-office # Will prompt for a password to complete install
 
